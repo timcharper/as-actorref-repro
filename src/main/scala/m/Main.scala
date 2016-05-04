78fce5f3ff9c6303c1e030805976caf55baee853
Tim Harper <timcharper@gmail.com>
2016-05-04 11:06:20 -0600

    first stab

--- src/main/scala/m/Main.scala
+++ src/main/scala/m/Main.scala
@@ -3,41 +3,109 @@ package m
 import akka.stream.scaladsl._
 import akka.stream._
 import akka.actor._
+import akka.stream.stage._
+import scala.collection.mutable
+
+trait IngestionMethods[A] {
+  def submit(value: A): Unit
+  def complete(): Unit
+}
+
+
+class Ingestor[A] extends GraphStageWithMaterializedValue[SourceShape[A], IngestionMethods[A]] {
+  /* We only have one thread dequeuing items, so it's safe to say that if the
+   * queue is not empty, then there will be an item available to pull.
+   */
+  private val buffer = new java.util.concurrent.ConcurrentLinkedQueue[A]
+
+  val out = Outlet[A]("Ingestor.out")
+  val shape = SourceShape.of(out)
+
+  private var downstreamWaiting = false
+  private var downstreamFinished = false
+  private var completing = false
+
+  override def createLogicAndMaterializedValue(inheritedAttributes: Attributes): (GraphStageLogic, IngestionMethods[A]) = {
+
+    class IngestorLogic(shape: Shape) extends GraphStageLogic(shape) with IngestionMethods[A] {
+      private val scheduled = new java.util.concurrent.atomic.AtomicBoolean(false)
+      private val processScheduled = getAsyncCallback[Unit] { (_) =>
+        if (!scheduled.compareAndSet(true, false)) {
+          throw new Exception("Code should never reach here. Error. Critical pain. No no no. Requesting repairs.")
+        }
+
+        if ((downstreamWaiting) && (! buffer.isEmpty)) {
+          downstreamWaiting = false
+          push(out, buffer.poll())
+        }
+
+      }
+      def submit(value: A): Unit = {
+        if (completing) return ()
+        buffer.offer(value)
+
+        /* if scheduled is still true after we've offered our value, then it is
+        guaranteed to be processed, because processing always occurs AFTER
+        transition from true to false */
+        if (scheduled.compareAndSet(false, true)) {
+          processScheduled.invoke(())
+        }
+      }
+
+      def complete(): Unit = {
+        completing = true
+        getAsyncCallback[Unit] { (_) =>
+          emitMultiple(out, buffer.iterator)
+          completeStage()
+        }.invoke(())
+      }
+
+      setHandler(out, new OutHandler {
+        override def onPull(): Unit = {
+          if (buffer.isEmpty) {
+            downstreamWaiting = true
+          } else {
+            push(out, buffer.poll())
+          }
+        }
+      })
+    }
+
+    val logic = new IngestorLogic(shape)
+
+    (logic, logic: IngestionMethods[A])
+  }
+}
 
 object Repro extends App {
   implicit val system = ActorSystem("test")
   implicit val materializer = ActorMaterializer()
   implicit val ec = system.dispatcher
 
-  println("""
-In this example, the messages sent immediately after the actorRef is returned
-are being dropped. However, after waiting 100ms, then sending a set more, 16
-messages of them make it through the pipeline (I presume 16 is the async
-boundary buffer size between the actorRef and the sink).
-
-The fact that only 16 make it through is evidence that the Sink is running in a
-separate thread than the receiving actor (whose mailbox contains the originally
-sent messages).
-""")
-  val (input, completed) = Source.actorRef[Int](0, OverflowStrategy.dropNew).
+  val (input, completed) = Source.fromGraph(new Ingestor[Int]).
     toMat(Sink.foreach { n =>
+      Thread.sleep(1)
       println(s"Processed ${n}")
     })(Keep.both).
     run
 
   (1 to 10).foreach { n =>
-    input ! n
+    Thread.sleep(10)
+    input.submit(n)
     println(s"Sent ${n}")
   }
-
-  Thread.sleep(100)
-
   (11 to 50).foreach { n =>
-    input ! n
+    input.submit(n)
     println(s"Sent ${n}")
   }
 
-  input ! Status.Success("ok")
+  input.complete()
+  println(s"Sent complete")
+
+  (51 to 100).foreach { n =>
+    input.submit(n)
+    println(s"Sent ${n}")
+  }
 
   completed.onComplete { result =>
     println(s"all done! Result = ${result}")
